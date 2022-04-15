b00acc62d1bef655867292bde3cdb1468464591e
drpianoman <wangry@gmail.com>
2022-04-14 20:34:23 -0400

    Uploaded Enterprise Escape Room
    Here's the unity project

---
+++ Enterprise Escape Room/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySound()
+    {
+        audioSource.Play();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
---
+++ Enterprise Escape Room/Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public List<GameObject> uIMenus = new List<GameObject>();
+
+    public void SwitchUI(int partNum)
+    {
+        for (int i = 0; i < uIMenus.Count; i++)
+        {
+            if (i == partNum)
+            {
+                uIMenus[i].SetActive(true);
+
+            }
+            else
+            {
+                uIMenus[i].SetActive(false);
+            }
+
+        }
+    }
+}
