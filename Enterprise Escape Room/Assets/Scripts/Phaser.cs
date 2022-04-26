a9aebad0846d786fd320faacc3024b67f020b275
drpianoman <wangry@gmail.com>
2022-04-26 18:59:43 -0400

    phaser functionality to stun tribbles to prevent them from breeding

--- Enterprise Escape Room/Assets/Scripts/Phaser.cs
+++ Enterprise Escape Room/Assets/Scripts/Phaser.cs
@@ -9,13 +9,16 @@ public class Phaser : XRGrabInteractable
     private LineRenderer line;
     public Transform startPoint;
     public AudioClip phaserSound;
-
+    public Transform hitTribble;
     private AudioSource audioSource;
+
+    RaycastHit hit;
 
     void Start()
     {
         activated.AddListener(FirePhaser);
         audioSource = GetComponent<AudioSource>();
+        line = GetComponent<LineRenderer>();
     }
 
     private void FirePhaser(ActivateEventArgs args)
@@ -29,13 +32,18 @@ public class Phaser : XRGrabInteractable
 
     public bool GetRaycastHit()
     {
-        RaycastHit hit;
+        
 
         if(Physics.Raycast(startPoint.position,transform.forward,out hit))
         {
             line.SetPosition(0, startPoint.position);
             line.SetPosition(1, hit.point);
             line.enabled = true;
+            if (hit.transform.CompareTag("Tribble"))
+            {
+                Tribble tribble = hit.transform.GetComponent<Tribble>();
+                tribble.doDuplicate = false;
+            }
             return true;
         }
         else
